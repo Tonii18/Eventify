7904e0e9d7ba14d4ef6a061a7a9c36bb273af8ed
Tonii18 <ajsanrosal@gmail.com>
2025-11-26 20:39:22 +0100

    Events page and floating action buttons done and working

---
+++ lib/views/users/components/speed_dial_fab.dart
@@ -0,0 +1,48 @@
+import 'package:eventify/config/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+
+class SpeedDialFab extends StatefulWidget {
+  const SpeedDialFab({super.key});
+
+  @override
+  State<SpeedDialFab> createState() => _SpeedDialFabState();
+}
+
+class _SpeedDialFabState extends State<SpeedDialFab> {
+  @override
+  Widget build(BuildContext context) {
+
+    final size = MediaQuery.of(context).size;
+    final scale = size.width / 400;
+
+    return SpeedDial(
+      icon: Icons.filter_alt_outlined,
+      animatedIconTheme: IconThemeData(size: 30 * scale),
+      backgroundColor: AppColors.primaryPurple,
+      foregroundColor: AppColors.white,
+      visible: true,
+      curve: Curves.bounceInOut,
+      children: [
+        SpeedDialChild(
+          child: Icon(Icons.music_note, color: AppColors.darkBlue),
+          backgroundColor: AppColors.white,
+          onTap: (){},
+          shape: CircleBorder()
+        ),
+        SpeedDialChild(
+          child: Icon(Icons.sports_gymnastics_rounded, color: AppColors.darkBlue),
+          backgroundColor: AppColors.white,
+          onTap: () {},
+          shape: CircleBorder()
+        ),
+        SpeedDialChild(
+          child: Icon(Icons.cell_tower, color: AppColors.darkBlue),
+          backgroundColor: AppColors.white,
+          onTap: () {},
+          shape: CircleBorder()
+        ),
+      ],
+    );
+  }
+}
--- lib/views/users/user_events.dart
+++ lib/views/users/user_events.dart
@@ -1,4 +1,6 @@
 import 'package:eventify/config/measures.dart';
+import 'package:eventify/config/theme.dart';
+import 'package:eventify/views/users/components/speed_dial_fab.dart';
 import 'package:eventify/views/widgets/base_page.dart';
 import 'package:flutter/material.dart';
 
@@ -12,13 +14,31 @@ class UserEvents extends StatefulWidget {
 class _UserEvents extends State<UserEvents> {
   @override
   Widget build(BuildContext context) {
+
+    // DONT DELETE
+
+    final size = MediaQuery.of(context).size;
+    final scale = size.width / 400;
+
     return Scaffold(
+
+      resizeToAvoidBottomInset: true,
+      backgroundColor: AppColors.greyBackground,
+
       body: BasePage(
         topMargin: Measures.marginTop, 
         child: SingleChildScrollView(
-          
+          child: Center(
+            child: Column(
+              children: [
+                
+              ],
+            ),
+          ),
         )
       ),
+
+      floatingActionButton: SpeedDialFab(),
     );
   }
 
